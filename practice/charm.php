2c4e2ae4037c2963b9485adf5363cf221847a37e
marieta@workabroad.ph <marieta@workabroad.ph>
2018-03-09 11:54:35 +0800

    Practices

---
+++ practice/charm.php
@@ -0,0 +1,85 @@
+<?php
+
+echo "sample commit";
+
+
+
+        if (
+            $a == $b &&
+            $b == $c &&
+            $c == $d &&
+            $g == $d
+        ) {
+            // if body
+        } elseif (
+            ($a == $b &&
+            $b == $c) &&
+            ($c == $d &&
+            $g == $d)
+        ) {
+            // elseif body
+        } else {
+            // else body
+        }
+
+
+
+define("constant", TRUE);
+$constantVariable = constant;
+
+
+
+
+       for($i = 0; 
+            $i < 10; $i++) {
+    // for body
+        }
+
+        foreach($iterable as $key => $value){
+    // foreach body
+}
+
+
+
+
+  FUNCTION someFunction($fname,$mname,$lname){
+                    
+                    return "kalimutan mo na yan sige sige mag libang wag ka mag pakahibang dapat ay itawa lng ang problema sa babae dapat di iniinda hayaan mo sila na maghabol sayu diba labyoooo";
+
+            }
+
+
+
+
+        switch($expr) 
+        {
+    case 0:
+        echo 'First case, with a break';
+            break;
+    case 1:
+        echo 'Second case, which falls through';
+        // no break
+    default:
+        echo 'Default case';
+        break;
+        }
+
+
+
+            
+
+            if($arg){
+echo "Hello World 1;
+
+            }else if($arg2){
+
+                echo Hello World 2";
+            }else{
+                echo "Hi Else"
+            }
+
+
+
+
+?>
+
---
+++ practice/kylie.php
@@ -0,0 +1,74 @@
+<?
+
+
+        switch($expr) 
+        {
+    case 0:
+        echo 'First case, with a break';
+            break;
+    case 1:
+        echo 'Second case, which falls through';
+        // no break
+    default:
+        echo 'Default case';
+        break;
+        }
+
+
+
+        if (
+            $a == $b &&
+            $b == $c &&
+            $c == $d &&
+            $g == $d
+        ) {
+            // if body
+        } elseif (
+            ($a == $b &&
+            $b == $c) &&
+            ($c == $d &&
+            $g == $d)
+        ) {
+            // elseif body
+        } else {
+            // else body
+        }
+
+
+define("constant", TRUE);
+$constantVariable = constant;
+
+
+  FUNCTION someFunction($fname,$mname,$lname){
+                    
+                    return "kalimutan mo na yan sige sige mag libang wag ka mag pakahibang dapat ay itawa lng ang problema sa babae dapat di iniinda hayaan mo sila na maghabol sayu diba labyoooo";
+
+            }
+
+            
+
+            if($arg){
+echo "Hello World 1;
+
+            }else if($arg2){
+
+                echo Hello World 2";
+            }else{
+                echo "Hi Else"
+            }
+
+
+        for($i = 0; 
+            $i < 10; $i++) {
+    // for body
+        }
+
+        foreach($iterable as $key => $value){
+    // foreach body
+}
+
+
+          
+
+?>
+
